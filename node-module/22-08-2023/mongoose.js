9029215cfc3faae1ce357c015e0ba3a141fc68d5
bhadreshce <bhadreshce17@gmail.com>
2023-08-22 08:46:11 +0530

    add

---
+++ node-module/22-08-2023/mongoose.js
@@ -0,0 +1,38 @@
+//database connection
+const mongoose = require('mongoose')
+const dbUrl = 'mongodb://127.0.0.1:27017/std'
+mongoose.set('strictQuery', true) //mongoose configuration
+mongoose.connect(dbUrl).then(() => {})
+
+// model creation for table
+
+const stdSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  marks: {
+    type: Number,
+  },
+})
+
+const stdModel = new mongoose.model('user', stdSchema)
+
+addData = new stdModel({
+  username: 'jayesh',
+  email: 'jayes1h@gmail.com',
+  age: 20,
+  marks: 30,
+})
+
+addData.save().then(() => {
+  console.log('succ')
+})
