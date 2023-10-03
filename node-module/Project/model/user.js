acd7d69c640add6ed0e6b7c786528cca06fce6a2
bhadreshce <bhadreshce17@gmail.com>
2023-10-03 16:46:44 +0530

    sq

--- node-module/Project/model/user.js
+++ node-module/Project/model/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -13,8 +13,14 @@ const userSchema = new mongoose.Schema({
   },
 })
 
-
-
+userSchema.pre('save', async function save(next) {
+  try {
+    this.password = await bcrypt.hash(this.password, 10)
+    return next()
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 const usermodel = new mongoose.model('user', userSchema)
 
--- node-module/Project/router/route.js
+++ node-module/Project/router/route.js
@@ -1,10 +1,36 @@
 const route = require('express').Router()
 
 const { usermodel } = require('../model/user')
-
+const bcrypt = require('bcryptjs')
 const { register } = require('../controller/register')
 route.get('/', (req, res) => {
+  res.render('login')
+})
+
+route.get('/do_register', (req, res) => {
   res.render('index')
+})
+
+route.post('/do_login', (req, res) => {
+  email = req.body.email
+  pass = req.body.password
+  console.log(email)
+
+  userdata = usermodel.findOne({ email: email }).then((data) => {
+    console.log(data)
+
+    if (!data) {
+      res.render('login')
+    } else {
+      isvalid = bcrypt.compare(req.body.password, data.password).then((pas) => {
+        if (pas) {
+          res.redirect('/user')
+        } else {
+          res.render('login')
+        }
+      })
+    }
+  })
 })
 
 route.get('/delete/(:id)', (req, res) => {
